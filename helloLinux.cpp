9d5d4001bcc3603603e6acaa91cba9d35b8df5a2
mohitsh <mohitsh114@gmail.com>
2015-09-07 00:12:20 +0530

    helloLinux.cpp

--- helloLinux.cpp
+++ helloLinux.cpp
@@ -1,10 +1,17 @@
 
 
-#include "iostream"
+#include <iostream>
 
 int main(void){
 	
 	std::cout << "Hello! Hello! Hello! is there anybody in here ? \n";
+	int x = 11;
+	std::cout << "Value of x is: " << x << "\n";
+	
+	std::cout << "This is line one" << std::endl;
+	std::cout << "this is line two" << std::endl;
+	std::cout << "this is line three" << std::endl;
+	std::cout << "this is line four" << std::endl;
 	return 0;
 
 }
