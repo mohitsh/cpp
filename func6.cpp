8a063329c246fc1bd29d39f7f8876ec147f31cb3
mohitsh <mohitsh114@gmail.com>
2015-09-07 13:15:16 +0530

    func6.cpp

---
+++ func6.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+int add(int x, int y){
+
+	return x+y;
+
+}
+
+int main(){
+
+	int a = 5;
+	int b = 6;
+	
+	std::cout << add(a,b) << " << hello, that was the answer" << std::endl;
+	return 0;
+}
