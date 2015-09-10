08a2fe996a91be8e763df5caf50e9581c05c1e24
mohitsh <mohitsh114@gmail.com>
2015-09-10 23:55:10 +0530

    division.cpp

--- division.cpp
+++ division.cpp
@@ -11,6 +11,14 @@ int main(){
 	std::cout << "int/double = " << x/static_cast<double>(y) << "\n";
 	std::cout << "double/double = " << static_cast<double>(x)/static_cast<double>(y) << "\n";
 	
+	int num = 1;
+	while (num <= 100){
+		std::cout << num  << " ";
+		if ( num %20 ==0){
+		std::cout << "\n";
+		}
+	num = num + 1;
+	}
 	return 0;
 
 }
