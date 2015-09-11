a29aeeab9bc822ee55b83573c551ad275c742c90
mohitsh <mohitsh114@gmail.com>
2015-09-11 17:56:16 +0530

    bitwise.cpp

--- bitwise.cpp
+++ bitwise.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <bitset>
 
 // while using bitwise operations always use unsigned integers
 
@@ -23,6 +24,37 @@ int main(){
 	int d2 = 12;	
 	int d3 = d1^d2;
 	std::cout << "or operator " << d3 << "\n";
+
+	
+	const unsigned char option1 = 0x01;
+	std::cout << option1 << "\n";
+	const unsigned char option2 = 0x02;
+	std::cout << option2 << "\n";
+	const unsigned char option7 = 0x08;
+	std::cout << option7 << "\n";
+	const unsigned char option8 = 0x80;
+	std::cout << option8 << "\n";
+	
+	std::bitset<8> bits(0x2);
+	bits.set(4);
+	bits.flip(5);
+	bits.reset(5);
+	std::cout << "bit 4 has value " << bits.test(4) << "\n";
+	std::cout << "bit 5 has value " << bits.test(5) << "\n";
+	std::cout << "all the bits " << bits << "\n";
+
+	std::bitset<8> bits1(0x1);
+	std::cout << bits1 << "\n";
+	std::bitset<8> bits2(0x2);
+	std::cout << bits2 << "\n";
+	std::bitset<8> bits3(0x3);
+	std::cout << bits3 << "\n";
+	std::bitset<8> bit4(0x4);
+	std::cout << bit4 << "\n";
+	std::bitset<8> bit5(0x5);
+	std::cout << bit5 << "\n";
+	bit5.reset(0);
+	std::cout << bit5 << "\n";
 	return 0;
 
 }
