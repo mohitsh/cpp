45fda7bf83fa78ede1b7372f308cf8eb0b432735
mohitsh <mohitsh114@gmail.com>
2015-09-20 13:44:04 +0530

    dude6_1quiz.cpp

--- dude6_1quiz.cpp
+++ dude6_1quiz.cpp
@@ -33,4 +33,14 @@ int main(){
 	double avg = static_cast<double>(total)/num_students;
 	std::cout << avg << "\n";
 
+	const int elements = 5;
+	int points[elements] { 84, 92, 76, 81, 56 };
+	int max_score = 0;
+	std::cout << "max score before iteration: " << max_score << "\n";
+	for (auto &num:points){  // passing by reference saves a lot of space
+		if (num > max_score)
+			max_score = num;
+	}
+	std::cout << "max score after iteration: " << max_score << "\n";
+
 }
