31f9bf9a9819346638ab9f7b9f72c37b1a33b074
mohitsh <mohitsh114@gmail.com>
2015-09-29 22:03:48 +0530

    insertAtN.cpp

--- operator/linkedList/insertAtN.cpp
+++ operator/linkedList/insertAtN.cpp
@@ -21,6 +21,8 @@ int main(){
 	{
 		cout << "Enter the Node: ";
 		cin >> info;
+
+	// Entering your first node
 		if (head == NULL)
 		{
 			node *temp;
@@ -29,6 +31,7 @@ int main(){
 			temp->next = head;
 			head = temp;
 		}
+	// entering modes after first node
 		else
 		{
 			node *temp1;
@@ -44,6 +47,7 @@ int main(){
 		}
 	}
 	
+	// traversing and printing the linked list
 	node *temp2;
 	temp2 = head;
 	
@@ -57,13 +61,14 @@ int main(){
 		cout << "\n";
 	}
 
-	cout << "enter the inserttion index: ";
+	cout << "enter the insertion index: ";
 	int index;
 	cin >> index;
 	cout << "enter node: ";
 	int val;
 	cin >> val;
 	
+	// find the index to enter at
 	node *temp3;	
 	temp3 = (node*)malloc(sizeof(node));
 	temp3 = head;
@@ -75,12 +80,16 @@ int main(){
 			break;
 		}
 	}
+	// now make a node and insert at that index
+	if (temp3 != NULL){
 	node *temp4;
 	temp4 = (node*)malloc(sizeof(node));
 	temp4->data = val;
 	temp4->next = temp3->next;
 	temp3->next = temp4;
+	}
 
+	// traverse the linked list once again
 	node *temp5;
         temp5 = head;
 
@@ -94,7 +103,93 @@ int main(){
                 cout << "\n";
         }
 
+	// deleting from front
+	node *temp6;
+	temp6 = (node*)malloc(sizeof(node));
+	temp6=head;
+	head = temp6->next;
+	free(temp6);
 	
+	// traversing again
+	cout << "after deleting from front " << "\n";
+	node *temp7;
+	temp7 = head;
+	if (temp7 == NULL)
+		cout << "okay! No element present " << "\n";
+	else{
+		while (temp7 != NULL){
+			cout << temp7->data << " ";
+			temp7 = temp7->next;
+		}
+	}
+	cout << "\n";
+
+	// deleting from back
+	node *temp8;
+	temp8 = (node*)malloc(sizeof(node));
+	temp8 = head;
+	node *old_temp;
+	old_temp = (node*)malloc(sizeof(node));
+	while(temp8->next!=NULL){
+		old_temp = temp8;
+		temp8 = temp8->next;
+	} 
+	old_temp->next = NULL;
+	free(temp8);
+
+	// lets traverse again after deleting last node
+	cout << "after deleting last node ->" << "\n";
+	node *temp9;
+	temp9 = (node*)malloc(sizeof(node));
+	temp9 = head;
+	if (temp9 == NULL)
+		cout << "empty ";
+	else{
+		while(temp9!=NULL){
+			cout << temp9->data << " ";
+			temp9 = temp9->next;
+		}
+	}
+	cout << "\n";
+
+	// deleting node at a specific index;
+	
+	node *temp10;
+	temp10 = (node*)malloc(sizeof(node));
+	temp10 = head;
+	
+	node *old_temp1;
+	old_temp1 = (node*)malloc(sizeof(node));
+	old_temp1 = temp10;
+	
+	int del;
+	cout << "enter the node to be deleted: ";
+	cin >> del;
+	
+	for (int i = 1; i < del; i++){
+		old_temp1 = temp10;
+		temp10 = temp10->next;
+	}
+	old_temp1->next = temp10->next;
+	free(temp10);
+	
+	cout << "deleting node at a given index " << "\n";
+
+	node *temp11;
+	temp11 = (node*)malloc(sizeof(node));
+	temp11 = head; 
+	
+	if (temp11 == NULL)
+		cout << "empty" << "\n";
+	else{
+		while (temp11 != NULL){
+			cout << temp11->data << " ";
+			temp11 = temp11->next;
+		}
+	cout << "\n";
+	}	
+		
+
 }
 
 
