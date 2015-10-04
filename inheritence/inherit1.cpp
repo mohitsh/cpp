14f1437df4ac3db9ff17bfbc9d828c61a0ddfb7f
mohitsh <mohitsh114@gmail.com>
2015-10-04 22:45:40 +0530

    inherit1.cpp

--- inheritence/inherit1.cpp
+++ inheritence/inherit1.cpp
@@ -13,9 +13,61 @@ class Person
 		int GetAge(){return m_nAge;}
 		bool IsMale(){return m_bIsMale;}
 
+		Person(std::string strName = "", int age = 0, bool bIsMale = false)
+			: m_strName(strName), m_nAge(age), m_bIsMale(bIsMale)
+		{
+		}
+
+};
+
+class BaseballPlayer : public Person
+{
+
+	public:
+		double m_dBattingAverage;
+		int m_nHomeRuns;
+	
+	BaseballPlayer(double dBattingAverage = 0.0, int nHomeRuns = 0)
+		: m_dBattingAverage(dBattingAverage),m_nHomeRuns(nHomeRuns)
+		{
+		}
+};
+
+class Employee: public Person
+{
+	public:
+		string m_strEmployeeName;
+		double m_dHourlySalary;
+		long m_EmployeeID;
+		
+		Employee(string str_EmployeeName, double dHourlySalary, long EmployeeID)
+			: m_strEmployeeName(str_EmployeeName), m_dHourlySalary(dHourlySalary), m_EmployeeID(EmployeeID)
+		{
+		}
+		
+		double GetHourlySalary(){ return m_dHourlySalary;}
+		void PrintNameAndSalary()
+		{
+			cout << m_strName << ":" << m_dHourlySalary << endl;
+		}
 };
 
 int main()
 {
+
+	BaseballPlayer dude;
+	dude.m_strName = "bohra dude";
+	cout << dude.GetName() << endl;
+	Employee bohra;
+	bohra.PrintNameAndSalary
 	return 0;
 }
+
+
+
+
+
+
+
+
+
