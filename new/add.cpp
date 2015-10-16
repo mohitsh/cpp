0f141b232b1ee3f4ae65676393d8501ca2a6c4d4
mohitsh <mohitsh114@gmail.com>
2015-10-17 01:40:55 +0530

    main.cpp

---
+++ new/add.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int add(int x, int y)
+{
+
+	return x+y;
+}
+
---
+++ new/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int add(int x, int y);
+
+int main()
+{
+	using namespace std;
+	cout << add(51,6) << endl;
+}
+
+
