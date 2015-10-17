89566f8954578d978040c576a7756cad5c7c7181
mohitsh <mohitsh114@gmail.com>
2015-10-17 21:55:25 +0530

    headComb.h

---
+++ new/header1710/headComb.h
@@ -0,0 +1,9 @@
+
+#ifndef headComb
+#define headComb
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+
+#endif
---
+++ new/header1710/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "headComb.h"
+
+int main()
+{
+	using namespace std;
+	cout << add(20,10) << endl;
+	cout << sub(20,10) << endl;
+	cout << mul(20,10) << endl;
+
+	return 0;
+}
+
