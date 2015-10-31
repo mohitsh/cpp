af73686bbf461960cabb9cb7abe3829a3e84e4dd
mohitsh <mohitsh114@gmail.com>
2015-10-31 14:10:12 +0530

    problem1.cpp

--- worksApp/trial/problem1.cpp
+++ worksApp/trial/problem1.cpp
@@ -11,84 +11,95 @@
 
 using namespace std;
 
-vector<int> findbest(vector< vector<int> > &v, int i, int j, int row, int col)
+vector<int> turnRight(vector< vector<int> > &v, int i, int j, int row, int col)
 {	
 	//vector< vector<int> > v = v1;
 	vector<int> vec;
 		if (i == 0)
 		{
 			//cout << "dude I'm here!!!" << endl;
-			if (v[i+1][j] > v[i][j+1])
-			{
-				vec.push_back(i+1);
-				vec.push_back(j);
-				vec.push_back(v[i+1][j]);
-				v[i][j] = 0;
-			}
-			else
-			{
-				vec.push_back(i);
-				vec.push_back(j+1);
-				vec.push_back(v[i][j+1]);	
-				v[i][j] = 0;
-			}
-					
+			//if (v[i+1][j] > v[i][j+1])
+			vec.push_back(i);
+			vec.push_back(j+1);
 			return vec;
 		}
 		else if ( i == row-1)
 		{
-			//cout << "for last row: " << endl;
-			//cout << "i-1 " << i-1 << endl;
-			//cout << "j " << j << endl;
-			//cout << "i " << i << endl;
-			//cout << "j+1 " << j+1 <<endl;
-			//cout << v[i-1][j] << endl;
-			//cout << v[i][j+1] << endl;
-			if (v[i-1][j] > v[i][j+1])
-			{
-				//cout << "last row should be here! " << endl;
-				vec.push_back(i-1);
-				vec.push_back(j);
-				vec.push_back(v[i-1][j]);
-				v[i][j] = 0;
-			}
-			else
-			{
-				//cout << "No! here " << endl;
-				vec.push_back(i);
-				vec.push_back(j+1);
-				vec.push_back(v[i][j+1]);
-				v[i][j] = 0;
-			}
+			//if (v[i-1][j] > v[i][j+1])
+			vec.push_back(i);
+			vec.push_back(j+1);
 			return vec;
 		}
 		else if (i > 0 and i < row-1)
 		{
 			//cout << "HERE!!!!!" << endl;
-			if (v[i-1][j] >= v[i+1][j] and v[i-1][j] >= v[i][j+1]) 
-			{
-				vec.push_back(i-1);
-				vec.push_back(j);
-				vec.push_back(v[i-1][j]);
-				v[i][j] = 0;
-			}
-			else if (v[i+1][j] >= v[i-1][j] and v[i+1][j] >=v[i][j+1])
-			{
-				vec.push_back(i+1);
-				vec.push_back(j);
-				vec.push_back(v[i+1][j]);
-				v[i][j] = 0;
-			}
-			else if (v[i][j+1] >= v[i+1][j] and v[i][j+1] >= v[i-1][j])
-			{
-				vec.push_back(i);
-				vec.push_back(j+1);
-				vec.push_back(v[i][j+1]);
-				v[i][j] = 0;
-			}
+			//if (v[i-1][j] >= v[i+1][j] and v[i-1][j] >= v[i][j+1]) 
+			vec.push_back(i);
+			vec.push_back(j+1);
 			return vec;
-		}		
+		}	
+		
 }
+
+vector<int> turnUp(vector< vector<int> > &v, int i, int j, int row, int col)
+{
+        //vector< vector<int> > v = v1;
+        vector<int> vec;
+                if (i == 0)
+                {
+                        //cout << "dude I'm here!!!" << endl;
+                        //if (v[i+1][j] > v[i][j+1])
+                        vec.push_back(row-1);
+                        vec.push_back(j);
+                        return vec;
+                }
+                else if ( i == row-1)
+                {
+                        //if (v[i-1][j] > v[i][j+1])
+                        vec.push_back(i-1);
+                        vec.push_back(j);
+                        return vec;
+                }
+                else if (i > 0 and i < row-1)
+                {
+                        //cout << "HERE!!!!!" << endl;
+                        //if (v[i-1][j] >= v[i+1][j] and v[i-1][j] >= v[i][j+1]) 
+                        vec.push_back(i-1);
+                        vec.push_back(j);
+                        return vec;
+                }
+}
+
+vector<int> turnDown(vector< vector<int> > &v, int i, int j, int row, int col)
+{
+        //vector< vector<int> > v = v1;
+        vector<int> vec;
+                if (i == 0)
+                {
+                        //cout << "dude I'm here!!!" << endl;
+                        //if (v[i+1][j] > v[i][j+1])
+                        vec.push_back(i+1);
+                        vec.push_back(j);
+                        return vec;
+                }
+                else if ( i == row-1)
+                {
+                        //if (v[i-1][j] > v[i][j+1])
+                        vec.push_back(0);
+                        vec.push_back(j);
+                        return vec;
+                }
+                else if (i > 0 and i < row-1)
+                {
+                        //cout << "HERE!!!!!" << endl;
+                        //if (v[i-1][j] >= v[i+1][j] and v[i-1][j] >= v[i][j+1]) 
+                        vec.push_back(i+1);
+                        vec.push_back(j);
+                        return vec;
+                }
+
+}
+
 
 int search(vector< vector<int> > &v1, int row, int col)
 {
@@ -105,7 +116,7 @@ int search(vector< vector<int> > &v1, int row, int col)
 		vector<int> step;
 		while(c_col != col-1)
 		{
-			step = findbest(v,c_row,c_col,row,col);
+			step = turnDown(v,c_row,c_col,row,col);
 			c_row = step[0];
 			c_col = step[1];
 			sum = sum + step[2];
