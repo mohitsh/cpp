2b1c262ae01cdc841ec9ea7a8bbb89b3242e52d6
mohitsh114 <mohitsh114@gmail.com>
2017-08-18 23:21:35 +0530

    revering a singlyLL

---
+++ LL/reverseLL/reverseLL.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+struct node
+{
+	int data;
+	node *next;
+};
+
+node *insertNodeAtBack(node *);
+void displayLL(node *);
+node *reverseLL(node *);
+
+int main()
+{
+	node *head;
+	head = new node;
+	head = NULL;
+	head = insertNodeAtBack(head);
+	//displayLL(head);
+	head = reverseLL(head);
+	displayLL(head);
+}
+
+
+node *insertNodeAtBack(node *head)
+{
+	int n;
+	cout << setw(20) << "Number of nodes: ";
+	cin >> n;	
+
+	for (int i = 0; i<n; ++i)
+	{
+		int new_data;
+		cout << "Enter node data: ";
+		cin >> new_data;
+		
+		node *temp;
+		temp = new node;
+		temp->data = new_data;
+
+		if (head == NULL)
+		{
+			temp->next = head;
+			head = temp;
+		}
+		else
+		{
+			node *traver;
+			traver = head;
+			while(traver->next != NULL)
+				traver = traver->next;
+			temp->next = traver->next;
+			traver->next = temp;
+		}
+	}
+	return head;
+}
+
+void displayLL(node *head)
+{
+	node *temp;
+	temp = head;
+	
+	while(temp != NULL)
+	{
+		cout << temp->data << endl;
+		temp = temp->next;
+	}
+}
+
+node *reverseLL(node *head)
+{
+	node *curr, *next, *prev;
+	next = prev = NULL;
+	curr = head;
+
+	while(curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	head = prev;
+	return head;
+
+}
+
+
+
