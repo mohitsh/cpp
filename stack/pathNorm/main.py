2e13dc9eaaebf14f116564cf3199a54b7476bb65
mohitsh114 <mohitsh114@gmail.com>
2017-09-16 01:07:53 +0530

    final algorithm

--- stack/pathNorm/main.py
+++ stack/pathNorm/main.py
@@ -1,8 +1,6 @@
 from stack import Stack
 
-
-
-def pathNoarmalizer(path):
+def pathNormalizer(path):
     startsRoot = False
     if path.startswith("/"):
        startsRoot = True 
@@ -12,21 +10,50 @@ def pathNoarmalizer(path):
     not_folder = [".", ".."]
     back_store = []
     for elem in path:
+        #print "processing: {0}".format(elem)
         if elem not in not_folder:
+            #print "pushing on stack: {0}".format(elem)
             s.push(elem)
         elif elem == ".":
+            #print "don't do anything: {0}".format(elem)
             continue
         elif elem == "..":
             if not s.isEmpty():
-                s.pop()
+                popped = s.pop()
+                #print "popped: {0}".format(popped)
+                continue
             if s.isEmpty() and startsRoot:
-                return "invalid"
+                #print "path is invalid"
+                return -1
             if s.isEmpty() and not startsRoot:
+                #print "stack is empty appending {0}".format(elem)
                 back_store.append("..")
 
+    #print back_store
+    if back_store:
+        new_path = "/".join(back_store)
+        new_path += '/'
+        #print('Shortest Path: {0}'.format(new_path))
+        return new_path
+
+    recreate_path = []
+    while(not s.isEmpty()):
+        popped = s.pop()
+        recreate_path.append(popped)
+
+    if recreate_path:
+        recreate_path = recreate_path[::-1]
+        new_path = '/'.join(recreate_path)
+    else:
+        new_path = ''
+    #print('Shortest Path: {0}'.format(new_path))
+    return new_path
 
 if __name__ == "__main__":
-    print(s.isEmpty())
-    s.push(1)
-    s.push(1)
-    print(s.isEmpty())
+    path = "/"
+    print 'original path: {0}'.format(path)
+    new_path = pathNormalizer(path)
+    if new_path == -1:
+        print "PATH IS INVALID"
+    else:
+        print('Shortest Path: {0}'.format(new_path))
--- stack/pathNorm/stack.py
+++ stack/pathNorm/stack.py
@@ -1,15 +1,15 @@
-class Stack():
+class Stack:
     def __init__(self):
         self.__items = []
 
     def isEmpty(self):
         if self.__items:
-            return True
-        else:
             return False
+        else:
+            return True
 
     def push(self, item):
         self.__items.append(item)
 
-    def pop(self, item):
-        return item.pop()
+    def pop(self):
+        return self.__items.pop()
