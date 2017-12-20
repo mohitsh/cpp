760f6d8e1eebabcd611d994cb01210e74bc14e2d
mohitsh114 <mohitsh114@gmail.com>
2017-12-20 13:41:15 +0530

    overlapping intervals problem

--- Problems/overlappingIntervals/main.cpp
+++ Problems/overlappingIntervals/main.cpp
@@ -10,22 +10,67 @@ using namespace std;
 void mergeOverlap(vector<int> &v, int n)
 {
 
+  int s = (*max_element(v.begin(), v.end()))+1;
+  //cout << s << endl;
 
-  int s = *max_element(v.begin(), v.end());
-  cout << s << endl;
+  vector<int> marker(s);
 
-  vector<int> marker(s+1);
-
-  for (int i = 0; i<v.size(); ++i)
+  for (int i = 0; i<v.size(); i = i+2)
   {
-    
-    marker[v[i]] = 1;
+    int start = v[i];
+    int end = v[i+1];
+    //cout << start << " " << end << endl;
+
+    if (start == end)
+    {
+        // do not oveeride 1 since 1 means it is part of some overlapping range
+        if (marker[start] == 0) marker[start] = 2; // if 2 then left and right was equal
+    }
+    for (int j = start; j<end; ++j)
+    {
+        marker[j] = 1;
+    }
   }
 
-  for (int i = 0; i<marker.size(); ++i)
-    cout << marker[i] << " ";
+  //for (int i = 0; i<s; ++i)
+  //  cout << marker[i] << " ";
+  //cout << endl;
 
-  cout << endl;
+    vector<vector<int> > range; 
+    
+    for (int i = 0; i<s; ++i)
+    {
+        if (marker[i] == 2)  // start and end range is equal. [1,1]
+        {
+            vector<int> new_range = {i, i};
+            range.push_back(new_range);
+        }
+
+        else if (marker[i] == 1)
+        {
+            if (i-1>=0 && marker[i-1] == 1) // extend the range 
+            {
+                vector<int> last_range = range[range.size()-1]; // extract
+                last_range[1] = i+1; // change
+
+                range[range.size()-1] = last_range; // update
+            }
+
+            if (i-1 >= 0 && marker[i-1] != 1)
+            {
+                vector<int> new_range = {i, i+1};
+                range.push_back(new_range);
+                
+            }
+        }
+    }  
+
+    for (int i = 0; i<range.size(); ++i)
+    {
+        vector<int> elem = range[i];
+        cout << elem[0] << " " << elem[1] << " ";
+    }
+    
 
 }
 
@@ -56,11 +101,8 @@ int main()
     }
 
     mergeOverlap(v, n);
-
     cout << endl;
   }
-
-
 
   return 0;  
 }
